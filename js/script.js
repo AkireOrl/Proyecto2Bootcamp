7a2974356a02adc51ebeb18da396cd2acdc8a241
erika <om.erikaorlando@gmail.com>
2023-10-22 20:03:45 +0200

    Inicialización de proyecto, subida de la estructura básica en index.html y en styles.css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const buttonA = document.getElementById("buttonA");
+// Puedes agregar eventos a los botones aquí.
+
+// Ejemplo de interacción:
+buttonA.addEventListener("click", () => {
+    // Lógica cuando se presiona el botón A.
+});
+
+
+//let buttons = document.querySelectorAll('.button');
+//buttons.forEach(button => {
+    button.style.transform = 'rotate(45deg)';
+//});
+
+//document.getElementById('arrowup').style.transform = 'rotate(0deg)';
+//document.getElementById('arrowright').style.transform = 'rotate(90deg)';
+//document.getElementById('arrowdown').style.transform = 'rotate(180deg)';
+//document.getElementById('arrowleft').style.transform = 'rotate(270deg)';
