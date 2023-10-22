dd76423a3f66fd79ea3e5c9cffaae7e0498c7d73
erika <om.erikaorlando@gmail.com>
2023-10-22 21:41:04 +0200

    Botón start cambia el fondo y hace sonido.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-const buttonA = document.getElementById("buttonA");
-// Puedes agregar eventos a los botones aquí.
+function cambiarFondo() {
+    // Obtén el elemento div con la clase "screen"
+    var divScreen = document.querySelector(".screen");
 
-// Ejemplo de interacción:
-buttonA.addEventListener("click", () => {
-    // Lógica cuando se presiona el botón A.
-});
-
-
-//let buttons = document.querySelectorAll('.button');
-//buttons.forEach(button => {
-    button.style.transform = 'rotate(45deg)';
-//});
-
-//document.getElementById('arrowup').style.transform = 'rotate(0deg)';
-//document.getElementById('arrowright').style.transform = 'rotate(90deg)';
-//document.getElementById('arrowdown').style.transform = 'rotate(180deg)';
-//document.getElementById('arrowleft').style.transform = 'rotate(270deg)';
+    // Cambia el fondo del div
+    divScreen.style.backgroundImage = 'url("./img/EntradaNintendo.png")';
+    divScreen.style.backgroundSize = "cover";
+    // Crea un elemento de audio
+    var audio = new Audio("./Sounds/gameboy.mp3");
+    // Reproduce el sonido
+    audio.play();
+  }
