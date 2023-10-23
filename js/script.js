619f5e8471f42c9a7838b9df5a4fcfd2717fa4b4
erika <om.erikaorlando@gmail.com>
2023-10-23 13:15:27 +0200

    Cambios para tercera pantalla con #buttonB-reproduce un video

--- js/script.js
+++ js/script.js
@@ -24,3 +24,22 @@ function cambiarFondo() {
     // Reproduce el sonido
     audio.play();
 });
+
+document.querySelector("#buttonB").addEventListener("click", function () {
+  // Obtén el elemento div con la clase "screen"
+  var divScreen = document.querySelector(".screen");
+
+  // Cambia el fondo del div
+  divScreen.innerHTML = '<video id="myVideo" autoplay loop muted poster="./img/poster.jpg"><source src="./img/video_2023-10-23_12-32-17.mp4" type="video/mp4"></video>';
+  divScreen.style.backgroundSize = "cover";
+  
+  // Crea un elemento de audio
+  var audio = new Audio(""); // Reemplaza con la ruta de tu nuevo archivo de sonido
+  // Reproduce el sonido
+  audio.play();
+
+  // Aplica estilos al video
+  var video = document.getElementById("myVideo");
+  video.style.width = "100%";
+  video.style.height = "100%";
+});
