9c984da134d8e2a4d16a84029d1a624c72285938
erika <om.erikaorlando@gmail.com>
2023-10-23 11:18:00 +0200

    Al pulsar #buttonA cambia el .screen y hay música

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function cambiarFondo() {
     // Reproduce el sonido
     audio.play();
   }
+
+  document.querySelector("#buttonA").addEventListener("click", function () {
+    // Obtén el elemento div con la clase "screen"
+    var divScreen = document.querySelector(".screen");
+
+    // Cambia el fondo del div
+    divScreen.style.backgroundImage = 'url("./img/tetris.jpg")'; // Reemplaza con la ruta de tu nueva imagen de fondo
+    divScreen.style.backgroundSize = "cover";
+    
+    // Crea un elemento de audio
+    var audio = new Audio("./Sounds/tetrisgameboy1-gameboy.mp3"); // Reemplaza con la ruta de tu nuevo archivo de sonido
+    // Reproduce el sonido
+    audio.play();
+});
