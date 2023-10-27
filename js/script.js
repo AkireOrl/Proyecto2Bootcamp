9d001b31b10cb5824e37188423d60aa919eb0cbf
erika <om.erikaorlando@gmail.com>
2023-10-27 19:03:55 +0200

    Cambio de funcion en el botón start

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
+// Variable para rastrear el estado del fondo
+var fondoCambiado = false;
+
 function cambiarFondo() {
-    
     var divScreen = document.querySelector(".screen");
 
-    // Cambia el fondo del div
-    divScreen.style.backgroundImage = 'url("./img/EntradaNintendo.png")';
-    divScreen.style.backgroundSize = "cover";
-    // Crea un elemento de audio
-    var audio = new Audio("./Sounds/gameboy.mp3");
-    // Reproduce el sonido
-    audio.play();
-  }
+    if (!fondoCambiado) {
+        // Cambia el fondo del div
+        divScreen.style.backgroundImage = 'url("./img/EntradaNintendo.png")';
+        divScreen.style.backgroundSize = "cover";
+        // Crea un elemento de audio
+        var audio = new Audio("./Sounds/gameboy.mp3");
+        // Reproduce el sonido
+        audio.play();
+
+        // Cambia el estado a true para indicar que el fondo ha sido cambiado
+        fondoCambiado = true;
+    } else {
+        // Vuelve al fondo original
+        divScreen.style.backgroundImage = 'url("./img/fondo-original.jpg")';
+        // Detén el sonido si se está reproduciendo
+        audio.pause();
+
+        // Restablece el estado a false para la próxima vez que se haga clic
+        fondoCambiado = false;
+    }
+}
 
   document.querySelector("#buttonA").addEventListener("click", function () {
     
