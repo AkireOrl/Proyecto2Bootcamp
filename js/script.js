efc7f293eab1a25a99a904a4c78661049d050317
erika <om.erikaorlando@gmail.com>
2023-10-30 20:23:49 +0100

    Arreglo de la manera de llamar al video - Cambio de HtmlInnet a append

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Variable para rastrear el estado del fondo
 var fondoCambiado = false;
 
 function cambiarFondo() {
@@ -18,42 +17,59 @@ function cambiarFondo() {
     } else {
         // Vuelve al fondo original
         divScreen.style.backgroundImage = 'url("./img/fondo-original.jpg")';
-        // Detén el sonido si se está reproduciendo
-        audio.pause();
 
         // Restablece el estado a false para la próxima vez que se haga clic
         fondoCambiado = false;
     }
 }
 
-  document.querySelector("#buttonA").addEventListener("click", function () {
-    
-    var divScreen = document.querySelector(".screen");
+document.querySelector("#buttonA").addEventListener("click", function () {
+    if (fondoCambiado) {
+        var divScreen = document.querySelector(".screen");
 
-    // Cambia el fondo del div
-    divScreen.style.backgroundImage = 'url("./img/tetris.jpg")';
-    divScreen.style.backgroundSize = "cover";
-    
-    // Crea un elemento de audio
-    var audio = new Audio("./Sounds/tetrisgameboy1-gameboy.mp3");
-    // Reproduce el sonido
-    audio.play();
+        // Cambia el fondo del div
+        divScreen.style.backgroundImage = 'url("./img/tetris.jpg")';
+        divScreen.style.backgroundSize = "cover";
+
+        // Crea un elemento de audio
+        // var audio = new Audio("./Sounds/tetrisgameboy1-gameboy.mp3");
+        // Reproduce el sonido
+        //audio.play();
+    } else {
+        var divScreen = document.querySelector(".screen");
+
+        // Cambia el fondo del div
+        divScreen.style.backgroundImage = 'url("./img/fondo-original.jpg")';
+
+        // Restablece el estado a false para la próxima vez que se haga clic
+        fondoCambiado = false;
+    }
+
 });
 
 document.querySelector("#buttonB").addEventListener("click", function () {
-  
-  var divScreen = document.querySelector(".screen");
+    if (fondoCambiado) {
+        var divScreen = document.querySelector(".screen");
+        var mivideo = document.createElement("video")
+        divScreen.append (mivideo)
 
-  // Cambia el fondo del div
-  divScreen.innerHTML = '<video id="myVideo" autoplay loop muted poster="./img/poster.jpg"><source src="./img/video_2023-10-23_12-32-17.mp4" type="video/mp4"></video>';
-  divScreen.style.backgroundSize = "cover";
-  
-  // Crea un elemento de audio
-  var audio = new Audio(""); 
-  audio.play();
+        mivideo.src = "./img/video_2023-10-23_12-32-17.mp4";
+        // Cambia el fondo del div
+       // divScreen.innerHTML = '<video id="myVideo" autoplay loop muted poster="./img/fondo-original.jpg"><source src="./img/video_2023-10-23_12-32-17.mp4" type="video/mp4"></video>';
+        divScreen.style.backgroundSize = "cover";
 
-  // Aplica estilos al video
-  var video = document.getElementById("myVideo");
-  video.style.width = "100%";
-  video.style.height = "100%";
+        // Aplica estilos al video
+    
+        mivideo.style.width = "100%";
+        mivideo.style.height = "100%";
+
+    } else {
+        var divScreen = document.querySelector(".screen");
+
+        // Cambia el fondo del div
+        divScreen.style.backgroundImage = 'url("./img/fondo-original.jpg")';
+
+        // Restablece el estado a false para la próxima vez que se haga clic
+        fondoCambiado = false;
+    }
 });
